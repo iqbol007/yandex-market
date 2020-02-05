efd265fdbf6bbf112f8825440fc40f268062f9d4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-05 07:29:25 +0000

    Deploy iqbol007/yandex-market to github.com/iqbol007/yandex-market.git:gh-pages

---
+++ precache-manifest.1cb68eac026d680013c7d09cc84903d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ab2d44ff4f9ec55e9dc92497f81825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c554a02bc57881471f",
+    "url": "/static/css/main.4996155e.chunk.css"
+  },
+  {
+    "revision": "bc7756e8ca804652da62",
+    "url": "/static/js/2.f7b5f6ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f7b5f6ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83c554a02bc57881471f",
+    "url": "/static/js/main.d750d50b.chunk.js"
+  },
+  {
+    "revision": "ed870dcd838767fd2978",
+    "url": "/static/js/runtime-main.afc67a08.js"
+  }
+]);
